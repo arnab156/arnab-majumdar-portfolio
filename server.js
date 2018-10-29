f99a2c2020c15f85d0bc384226d38c1bdfa7f52b
arnab majumdar <arnab156@gmail.com>
2018-10-29 15:13:43 -0500

    pkg json

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
-// const PORT =  3001;
+var db = require("./models");
 
 
 // Define middleware here
@@ -22,7 +22,7 @@ app.use(routes);
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/arnab_contacts", { useNewUrlParser: true });
 
 
-var db = require("./models");
+
 
 // db.Contacts.create(
 //   {name: "Arnab Majumdar" }, 
