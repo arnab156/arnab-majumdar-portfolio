3ecb99f3297924ccc7ee73c23ae4f10324c23e2c
arnab majumdar <arnab156@gmail.com>
2018-10-29 20:57:54 -0500

    pack jsob

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
-var db = require("./models");
+// var db = require("./models");
 
 
 // Define middleware here
